295e61f3d184015dfa3c8f6717de77f51dde6215
Lara <jlara@usaid.gov>
2023-11-21 11:29:30 +0200

    * improvements to plot_nat_trend & plot_nat_mult_trend

--- Scripts/dhs_api_2.R
+++ Scripts/dhs_api_2.R
@@ -25,7 +25,7 @@ load_secrets()
 # FUNCTIONS ---------------------------------------------------------------
 
 
-plot_nat_trend <- function(indicator, color_hex, type = "Percent") {
+plot_nat_trend <- function(indicator, color_hex, type = "Percent", c_interval = TRUE) {
   
   # prepare df for plot and define whether the values should be percent or absolute
   # valid type values are "Percent" and "Absolute"
@@ -103,6 +103,19 @@ plot_nat_trend <- function(indicator, color_hex, type = "Percent") {
     
   }
   
+  
+  if (c_interval == TRUE) {
+    
+    p <- p +
+      
+      geom_ribbon(aes(ymin = ci_low, ymax = ci_high), alpha = .1)
+    
+  } else if (c_interval == FALSE) {
+    
+    p <- p
+    
+  }
+  
   p <- p +
     labs(x = "",
          y = "",
@@ -114,14 +127,33 @@ plot_nat_trend <- function(indicator, color_hex, type = "Percent") {
 
 }
 
-plot_nat_mult_trend <- function(indicators, title, subtitle) {
+plot_nat_mult_trend <- function(indicators, type = "Percent", title, subtitle) {
   
   df <- dhs_data(indicatorIds = indicators,
                  countryIds = c("MZ"),
                  surveyYearStart = 1997) %>% 
-    select(CountryName, SurveyType, SurveyYear, Indicator, Value, CIHigh, CILow) %>% 
-    mutate(Value = Value / 100) %>% 
-    clean_names()
+    select(CountryName, SurveyType, SurveyYear, Indicator, Value, CIHigh, CILow) 
+    
+    if (type == "Percent") {
+      
+      df <- df |> 
+        
+        mutate(Value = ifelse(is.na(Value), NA, Value / 100),
+               CIHigh = ifelse(CIHigh == "", NA, CIHigh / 100),
+               CILow = ifelse(CILow == "", NA, CILow / 100))
+      
+    } else if (type == "Absolute") {
+      
+      df <- df |> 
+        
+        mutate(Value = ifelse(is.na(Value), NA, Value),
+               CIHigh = ifelse(CIHigh == "", NA, CIHigh),
+               CILow = ifelse(CILow == "", NA, CILow))
+      
+    }
+    
+  df <- clean_names(df)
+  
   
   p <- df %>% 
     ggplot(aes(survey_year, value, color = indicator)) +
@@ -145,8 +177,8 @@ plot_nat_mult_trend <- function(indicators, title, subtitle) {
     scale_y_continuous(expand = expansion(mult = 0.1)) + # ensures geom_text data labels are not cut off
     labs(x = "",
          y = "",
-         title = title_plot,
-         subtitle = subtitle_plot,
+         title = title,
+         subtitle = subtitle,
          caption = "Source: https://www.statcompiler.com/")
   
   return(p)
@@ -169,31 +201,33 @@ indicator_focus <- indicators %>%
 
 
 indicator_list <- c("CM_ECMT_C_U5M", "CM_ECMT_C_NNR", "CM_ECMT_C_IMR")
-title_plot <- "Early Childhood Mortality"
-subtitle_plot <- "Neonatal, Infant, and Child Mortality (5 year periods)"
+
 
 # plot of neonatal, infant, and child mortality
 plot_nat_mult_trend(indicators = indicator_list,
-                    title = "Early Child Mortality")
+                    type = "Absolute",
+                    title = "Early Child Mortality",
+                    subtitle = "Neonatal, Infant, and Child Mortality (5 year periods)")
+
 
 # plots of individual early childhood mortality indicators
 plot_nat_trend(indicator = "CM_ECMT_C_NNR",
                color_hex = "#00C19A",
-               type = "Absolute") +
-  geom_ribbon(aes(ymin = ci_low, ymax = ci_high), alpha = .1)
+               type = "Absolute",
+               c_interval = TRUE)
 
 plot_nat_trend(indicator = "CM_ECMT_C_IMR",
-               color_hex = "#8494FF") +
-  geom_ribbon(aes(ymin = ci_low, ymax = ci_high), alpha = .1)
+               color_hex = "#8494FF",
+               c_interval = TRUE)
 
 plot_nat_trend(indicator = "CM_ECMT_C_CMR",
-               color_hex = "#ABA300") +
-  geom_ribbon(aes(ymin = ci_low, ymax = ci_high), alpha = .1)
+               color_hex = "#ABA300",
+               c_interval = TRUE)
 
-
-# plot of 
+# plot of stunting
 plot_nat_trend(indicator = "CN_NUTS_C_HA2",
-               color_hex = "#E68613") +
+               color_hex = "#E68613",
+               c_interval = FALSE) +
   scale_y_continuous(labels = percent,
                      limits = c(0, .5))
 
