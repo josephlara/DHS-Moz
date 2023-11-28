2f6f0b72437355ec1a9a3ece7ae962bd2e544145
josephlara <joseph.lara@gmail.com>
2023-11-28 17:52:03 +0200

    * create plot_nat_disag for plotting individual indicators by disaggreation

--- Scripts/dhs_api_2.R
+++ Scripts/dhs_api_2.R
@@ -22,6 +22,17 @@ library(rdhs)
 load_secrets()
 
 
+# DHS INDICATOR SELECTION --------------------------------------------------------
+
+
+tags <- dhs_tags()
+indicators <- dhs_indicators()
+
+indicator_focus <- indicators %>% 
+  select(TagIds, Label, IndicatorId) %>% 
+  filter(TagIds == 36)
+
+
 # FUNCTIONS ---------------------------------------------------------------
 
 
@@ -185,17 +196,110 @@ plot_nat_mult_trend <- function(indicators, type = "Percent", title, subtitle) {
   
 }
 
+plot_nat_disag <- function(indicator, survey_year, breakdown, palette = "rocket", reverse_ord = FALSE, type = "Percent") {
+  
+  # prepare df for plot and define whether the values should be percent or absolute
+  # valid type values are "Percent" and "Absolute"
+  
+  df <- dhs_data(indicatorIds = indicator,
+                 countryIds = c("MZ"),
+                 surveyYear	= survey_year,
+                 breakdown = "all") |> 
+    select(CountryName, SurveyType, SurveyYear, Indicator, CharacteristicCategory, CharacteristicLabel, Value, CIHigh, CILow) |> 
+    filter(CharacteristicCategory == breakdown)
+  
+  if (type == "Percent") {
+    
+    df <- df |> 
+      
+      mutate(Value = ifelse(is.na(Value), NA, Value / 100),
+             CIHigh = ifelse(CIHigh == "", NA, CIHigh / 100),
+             CILow = ifelse(CILow == "", NA, CILow / 100))
+    
+  } else if (type == "Absolute") {
+    
+    df <- df |> 
+      
+      mutate(Value = ifelse(is.na(Value), NA, Value),
+             CIHigh = ifelse(CIHigh == "", NA, CIHigh),
+             CILow = ifelse(CILow == "", NA, CILow))
+    
+  }
+  
+  df <- clean_names(df)
+  
+  
+  # fetch plot label and subtitle text values from "indicators" object
+  indicator_label <- filter(indicators, IndicatorId == indicator)$Label
+  indicator_definition <- filter(indicators, IndicatorId == indicator)$Definition
+  
+  # plot indicator
+  p <- df |> 
+    ggplot(aes(x = fct_reorder(characteristic_label, value, .desc = reverse_ord), y = value, fill = value)) +
+    scale_fill_viridis(option = palette, direction = -1, end = .8) +
+    geom_col(alpha = .75) +
+    si_style_xgrid() +
+    coord_flip() +
+    theme(plot.background = element_rect(fill = "#e7e7e5", colour = "#e7e7e5"),
+          plot.title = element_text(size = 16, vjust = 3),
+          plot.subtitle = element_text(size = 8, vjust = 6, color = "grey40"),
+          plot.caption = element_text(face = "italic", size = 9, vjust = 1),
+          panel.spacing = unit(.75, "cm"),
+          axis.text.x = element_text(size = 9, angle = 45, vjust = 1, hjust=1),
+          legend.position = "none",
+          legend.direction = "vertical",
+          legend.title = element_blank())
+  
+  
+  if (type == "Percent") {
+    
+    p <- p +
+      
+      geom_text(aes(label = scales::percent(value, 1)),
+                size = 3,
+                hjust = -.25,
+                vjust = .5) +
+      scale_y_continuous(labels = percent,
+                         expand = expansion(mult = 0.1)) 
+    
+  } else if (type == "Absolute") {
+    
+    p <- p +
+      
+      geom_text(aes(label = value),
+                size = 3,
+                hjust = -.25,
+                vjust = .5) +
+      scale_y_continuous(expand = expansion(mult = 0.1)) 
+    
+  }
+  
+  p <- p +
+    labs(x = "",
+         y = "",
+         title = indicator_label,
+         subtitle = str_wrap(indicator_definition, width = 115),
+         caption = "Source: https://www.statcompiler.com/")
+  
+  return(p)
+  
+}
 
-# DHS INDICATOR SELECTION --------------------------------------------------------
+# BREADOWN CHOICES
+# [1] "Total"                       "Sex"                         "Age in months"              
+# [4] "Residence"                   "Education"                   "Education (2 groups)"       
+# [7] "Wealth quintile"             "Mother's interview status"   "Size at birth"              
+# [10] "Birth interval"              "Mother's nutritional status" "Region"   
 
-
-tags <- dhs_tags()
-indicators <- dhs_indicators()
-
-indicator_focus <- indicators %>% 
-  select(TagIds, Label, IndicatorId) %>% 
-  filter(TagIds == 14)
-
+# PALETTE CHOICES
+# "magma" (or "A")
+# "inferno" (or "B")
+# "plasma" (or "C")
+# "viridis" (or "D")
+# "cividis" (or "E")
+# "rocket" (or "F")
+# "mako" (or "G")
+# "turbo" (or "H")
 
 # EARLY CHILDHOOD MORTALITY VISUALIZATION -------------------------------------------------
 
@@ -216,13 +320,18 @@ plot_nat_trend(indicator = "CM_ECMT_C_NNR",
                type = "Absolute",
                c_interval = TRUE)
 
+
 plot_nat_trend(indicator = "CM_ECMT_C_IMR",
                color_hex = "#8494FF",
+               type = "Absolute",
                c_interval = TRUE)
+
 
 plot_nat_trend(indicator = "CM_ECMT_C_CMR",
                color_hex = "#ABA300",
+               type = "Absolute",
                c_interval = TRUE)
+
 
 # plot of stunting
 plot_nat_trend(indicator = "CN_NUTS_C_HA2",
@@ -231,4 +340,12 @@ plot_nat_trend(indicator = "CN_NUTS_C_HA2",
   scale_y_continuous(labels = percent,
                      limits = c(0, .5))
 
+
+# plot of stunting by breakdown option
+plot_nat_disag(indicator = "CN_NUTS_C_HA2", 
+               survey_year = 2011, 
+               breakdown = "Region", 
+               palette = "plasma",
+               reverse_ord = FALSE, 
+               type = "Percent")
 
